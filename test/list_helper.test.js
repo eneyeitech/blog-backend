e6cbb7c5ddceeb9365367a3712e706df0845e37b
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2025-08-02 23:10:34 +0100

    4.6*: Helper Functions and Unit Tests, step 4 Implement and test passed

--- test/list_helper.test.js
+++ test/list_helper.test.js
@@ -1,6 +1,6 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
-const { totalLikes, favoriteBlog } = require('../utils/list_helper')
+const { totalLikes, favoriteBlog, mostBlogs } = require('../utils/list_helper')
 
 describe('total likes', () => {
     test('of empty lists is zero', () => {
@@ -89,7 +89,45 @@ describe("favorite", () => {
             }, h
             
         ]
+        
         const result = favoriteBlog(listWithMoreThanOneBlog)
         assert.deepStrictEqual(result, h)
+    })
+})
+
+describe("Top Authors", () => {
+    const h = {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'eneye',
+                url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                likes: 5,
+                __v: 0
+            };
+
+    test('author with most blogs 2', () => {
+        const listWithMoreThanOneBlog = [
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'eneye',
+                url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                likes: 3,
+                __v: 0
+            },
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                likes: 2,
+                __v: 0
+            }, h
+            
+        ]
+        const expected = {author: "eneye", blogs: 2}
+        console.log('end', expected)
+        const result = mostBlogs(listWithMoreThanOneBlog)
+        assert.deepStrictEqual(result, expected)
     })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,18 +8,42 @@ const totalLikes = (blogs) => {
     }
 
     return blogs.length === 0
-      ? 0
-      : blogs.reduce(reducer, 0)
+        ? 0
+        : blogs.reduce(reducer, 0)
 }
 
 const favoriteBlog = (blogs) => {
-    const highest = Math.max(...blogs.map((blog) => {return blog.likes}))
+    const highest = Math.max(...blogs.map((blog) => { return blog.likes }))
     console.log('high', highest)
     return blogs.find((blog) => blog.likes === highest)
+}
+
+const mostBlogs = (blogs) => {
+    const authors = blogs.map((blog) => blog.author)
+    console.log('author', authors)
+    const authorsBlogCount = authors.reduce((arr, author) => {
+        const index = arr.findIndex(o => o.author === author)
+        if (index === -1) {
+            arr.push({
+                "author": author,
+                blogs: 1
+            })
+        } else {
+            arr[index].blogs = arr[index].blogs + 1;
+            console.log('jj', index, arr)
+        }
+        return arr;
+    }, [])
+
+    const highest = Math.max(...authorsBlogCount.map((a) => a.blogs ))
+    console.log('high', highest)
+    return authorsBlogCount.find((a) => a.blogs === highest)
+    
 }
 
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
