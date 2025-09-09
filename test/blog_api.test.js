2d679b989ececed0675486383db279ead99123f4
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2025-09-09 17:11:56 +0100

    4.9: Blog List Tests, step 2 Implemented

--- test/blog_api.test.js
+++ test/blog_api.test.js
@@ -29,6 +29,14 @@ test('all blogs are returned', async () => {
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
+test('unique identifier property of blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  const blog = response.body[0]
+  assert.ok(blog.id)
+  assert.strictEqual(blog._id, undefined)
+})
+
+
 after(async () => {
   await mongoose.connection.close()
 })
