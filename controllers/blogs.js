e50428db4cd3d17c013b245f0670e09f552845d5
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2025-09-10 17:41:22 +0100

    4.17: Blog List Expansion, step 5 Implemented

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,9 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
-  response.json(blogs);
+   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs)
 })
 
 /**blogsRouter.get('/', (request, response) => {
@@ -19,14 +20,25 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).end();
   }
 
+ //const user = await User.findById(body.userId)
+  const user = await User.findOne()
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: user._id
   })
 
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog);
 })
 
--- controllers/users.js
+++ controllers/users.js
@@ -20,7 +20,7 @@ usersRouter.post('/', async (request, response) => {
 })
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({});/*.populate('notes', {content: 1, important: 1})*/
+  const users = await User.find({}).populate('blogs', { title: 1, url: 1, author: 1 })
   response.json(users)
 })
 
